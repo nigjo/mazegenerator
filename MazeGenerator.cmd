b857c7b845ac00892c6bf29cd8cb7f9528f687f1
Jens Hofschröer <github@nigjo.de>
2020-05-01 13:46:23 +0200

    defaultvalues no stored in %DEFAULT_*% vars (may be changed beforehand)

--- MazeGenerator.cmd
+++ MazeGenerator.cmd
@@ -1,12 +1,15 @@
 @echo off
+setlocal
 
+if not defined DEFAULT_WIDTH set DEFAULT_WIDTH=20
+if not defined DEFAULT_HEIGHT set DEFAULT_HEIGHT=16
 if not "%~1" == "auto" (
-  if not defined MAZE_WIDTH set /p "MAZE_WIDTH=Breite (15): "
-  if not defined MAZE_HEIGHT set /p "MAZE_HEIGHT=Hoehe (10): "
+  if not defined MAZE_WIDTH set /p "MAZE_WIDTH=Breite (%DEFAULT_WIDTH%): "
+  if not defined MAZE_HEIGHT set /p "MAZE_HEIGHT=Hoehe (%DEFAULT_HEIGHT%): "
   if not defined MAZE_SEED set /p "MAZE_SEED=Zufallscode ("%DATE%"): "
 )
-if not defined MAZE_WIDTH set MAZE_WIDTH=15
-if not defined MAZE_HEIGHT set MAZE_HEIGHT=10
+if not defined MAZE_WIDTH set MAZE_WIDTH=%DEFAULT_WIDTH%
+if not defined MAZE_HEIGHT set MAZE_HEIGHT=%DEFAULT_HEIGHT%
 if not defined MAZE_SEED set "MAZE_SEED=%DATE%"
 
 if not "%~1" == "auto" (
@@ -14,6 +17,6 @@ call bin\MazeGenerator.bat %MAZE_WIDTH% %MAZE_HEIGHT% "%MAZE_SEED%"
 ) else (
 echo "%MAZE_SEED%"
 call bin\MazeGenerator.bat %MAZE_WIDTH% %MAZE_HEIGHT% "%MAZE_SEED%"
-)>>%~n0.log
+)>>~n0.log
 
 if not "%~1" == "auto" pause

8d4b41c69f2b246744f0bffd38353bfb959148e0
Jens Hofschröer <github@nigjo.de>
2020-05-22 20:44:30 +0200

    added "--all" option to build a scoreboard about all Scorers

--- src/de.nigjo.maze.score/classes/de/nigjo/maze/score/Startup.java
+++ src/de.nigjo.maze.score/classes/de/nigjo/maze/score/Startup.java
@@ -50,23 +50,40 @@ public class Startup
       scorers.forEach(s -> System.out.println(s.getName()));
       return;
     }
+    List<String> hashes;
+    if("--all".equals(args[0]))
+    {
+      String[] shifted = Arrays.asList(args)
+          .subList(1, args.length)
+          .toArray(String[]::new);
+      Config cfg = new Config();
+      cfg.parseCommandline(shifted, 3);
+      int count = shifted.length > 2 ? Integer.parseInt(shifted[2]) : 1;
 
-    Config cfg = new Config();
-    cfg.parseCommandline(args, 3);
-    int count = args.length > 2 ? Integer.parseInt(args[2]) : 1;
+      hashes = performAllScores(cfg, count);
 
-    List<MazeInfo> mazes = MazeGenerationManager.generateMazes(cfg, count);
+    }
+    else
+    {
 
-    Collection<ScoreInfo> scores = findScores(mazes);
+      Config cfg = new Config();
+      cfg.parseCommandline(args, 3);
+      int count = args.length > 2 ? Integer.parseInt(args[2]) : 1;
 
-    ResultPrinter.printMazes(scores);
+      List<MazeInfo> mazes = MazeGenerationManager.generateMazes(cfg, count);
 
-    HashfileManager.store(scores);
+      Collection<ScoreInfo> scores = findScores(mazes);
+
+      ResultPrinter.printMazes(scores);
+      hashes = new ArrayList<>();
+      scores.forEach(i -> hashes.add(i.mazeInfo.hash));
+    }
+
+    HashfileManager.store(hashes);
   }
 
   private static Collection<ScoreInfo> findScores(List<MazeInfo> mazes)
   {
-    List<ScoreInfo> scores = new ArrayList<>();
     ServiceLoader<Scorer> scorers = ServiceLoader.load(Scorer.class);
     Scorer scorer = scorers.findFirst().orElseThrow();
     String config = System.getProperty("de.nigjo.maze.scorer");
@@ -82,6 +99,12 @@ public class Startup
         }
       }
     }
+    return findScores(mazes, scorer);
+  }
+
+  private static Collection<ScoreInfo> findScores(List<MazeInfo> mazes, Scorer scorer)
+  {
+    List<ScoreInfo> scores = new ArrayList<>();
     for(MazeInfo info : mazes)
     {
       ScoreInfo scoreData = scorer.getScores(info);
@@ -134,6 +157,38 @@ public class Startup
         - s2.scores.getOrDefault("score", 0.).doubleValue();
 
     return delta < 0 ? -1 : (delta > 0 ? 1 : 0);
+  }
+
+  private static List<String> performAllScores(Config cfg, int count)
+  {
+    Map<Scorer, Map<Long, ScoreInfo>> scorerData = new LinkedHashMap<>();
+    Map<Long, Integer> scoreboard = new HashMap<>();
+    ServiceLoader< Scorer> scorers = ServiceLoader.load(Scorer.class);
+    for(Scorer scorer : scorers)
+    {
+      List<MazeInfo> mazes = MazeGenerationManager.generateMazes(cfg, count);
+      Collection<ScoreInfo> scores = findScores(mazes, scorer);
+      Set<Number> harvester = new TreeSet<>();
+      scores.forEach(s -> harvester.add(s.scores.get("score")));
+      List<Number> punkte = new ArrayList<>(harvester);
+      for(ScoreInfo score : scores)
+      {
+        int punkt = scores.size() - punkte.size()
+            + punkte.indexOf(score.scores.get("score"));
+        scoreboard.merge(score.mazeInfo.seed, punkt, (o, n) -> o + n);
+      }
+      scorerData.put(scorer, scores.stream().collect(
+          Collectors.toMap(s -> s.mazeInfo.seed, Function.identity())));
+    }
+    List<Long> places = new ArrayList<>(scoreboard.keySet());
+    places.sort((l1, l2) -> scoreboard.get(l1) - scoreboard.get(l2));
+
+    ResultPrinter.printFullScores(places, scorerData);
+
+    List<String> hashes = new ArrayList<>();
+    Map<Long, ScoreInfo> data = scorerData.get(scorers.findFirst().orElseThrow());
+    places.forEach(seed -> hashes.add(data.get(seed).mazeInfo.hash));
+    return hashes;
   }
 
   private static class MazeGenerationManager
@@ -235,7 +290,7 @@ public class Startup
     private static final String HASHES_FILENAME =
         System.getProperty("de.nigjo.maze.score.hashesfile");
 
-    static void store(Collection<ScoreInfo> scores)
+    static void store(Collection<String> hashes)
     {
       if(HASHES_FILENAME != null)
       {
@@ -246,9 +301,9 @@ public class Startup
               StandardCharsets.UTF_8,
               StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING))
           {
-            for(ScoreInfo score : scores)
+            for(String hash : hashes)
             {
-              out.write(score.mazeInfo.hash);
+              out.write(hash);
               out.newLine();
             }
           }
@@ -337,23 +392,7 @@ public class Startup
         {
           scoreData.putAll(item.scores);
 
-          for(Map.Entry<String, Number> entry : scoreData.entrySet())
-          {
-            data.append(", ")
-                .append(entry.getKey())
-                .append(": ");
-            Number num = entry.getValue();
-            if(num instanceof Integer
-                || num instanceof Long
-                || num instanceof Short)
-            {
-              data.append(num);
-            }
-            else
-            {
-              data.append(String.format("%4.1f", num));
-            }
-          }
+          appendScores(scoreData, data);
         }
 
         out.println(levelView);
@@ -361,5 +400,87 @@ public class Startup
         out.println(data);
       }
     }
+
+    private static void appendScores(Map<String, Number> scoreData, StringBuilder data)
+    {
+      for(Map.Entry<String, Number> entry : scoreData.entrySet())
+      {
+        data.append(", ")
+            .append(entry.getKey())
+            .append(": ");
+        Number num = entry.getValue();
+        if(num instanceof Integer
+            || num instanceof Long
+            || num instanceof Short)
+        {
+          data.append(num);
+        }
+        else
+        {
+          data.append(String.format("%4.1f", num));
+        }
+      }
+    }
+
+    private static void printFullScores(List<Long> seedOrder,
+        Map<Scorer, Map<Long, ScoreInfo>> scorerData)
+    {
+      for(Long seed : seedOrder)
+      {
+        System.out.println("seed: " + seed);
+        List<String[]> lines = new ArrayList<>();
+        List<String[]> params = new ArrayList<>();
+        for(Map<Long, ScoreInfo> data : scorerData.values())
+        {
+          ScoreInfo info = data.get(seed);
+          lines.add(
+              QuadraticMazePainter.toString(info.mazeInfo.maze,
+                  '·', info.marker).split("\n"));
+          StringBuilder scoreValues = new StringBuilder();
+          Map<String, Number> scores = new TreeMap<>(ResultPrinter::sortWithScoreFirst);
+          scores.putAll(info.scores);
+
+          ResultPrinter.appendScores(scores, scoreValues);
+          params.add(scoreValues.toString().split(", "));
+        }
+
+        int lineCount = lines.get(0).length;
+        for(int i = 0; i < lineCount; i++)
+        {
+          StringBuilder b = new StringBuilder();
+          for(String[] line : lines)
+          {
+            b.append(' ').append(line[i]);
+          }
+          System.out.println(b);
+        }
+        int colsize = lines.get(0)[0].length();
+        char[] spaces = new char[colsize];
+        Arrays.fill(spaces, ' ');
+        for(int i = 0; i < lineCount; i++)
+        {
+          StringBuilder b = new StringBuilder();
+          for(String[] line : params)
+          {
+            b.append(' ');
+            if(i < line.length)
+            {
+              b.append(' ');
+              b.append(line[i]);
+              b.append(spaces, 0, colsize - line[i].length() - 1);
+            }
+            else
+            {
+              b.append(spaces, 0, colsize);
+            }
+          }
+          if(!b.toString().isBlank())
+          {
+            System.out.println(b);
+          }
+        }
+      }
+    }
+
   }
 }

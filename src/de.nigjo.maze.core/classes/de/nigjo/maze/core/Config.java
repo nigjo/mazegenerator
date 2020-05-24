5f4e9b56cc8f75790b9b3cc928dc84fe5e906e2f
Jens Hofschröer <github@nigjo.de>
2020-05-24 12:54:31 +0200

    advanced CLI management. A main class can define @Config.CliParameter

--- src/de.nigjo.maze.core/classes/de/nigjo/maze/core/Config.java
+++ src/de.nigjo.maze.core/classes/de/nigjo/maze/core/Config.java
@@ -15,17 +15,29 @@
  */
 package de.nigjo.maze.core;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
+ * A configuration to build a maze.
  *
  * @author nigjo
  */
 public class Config
 {
+  @CliParameter(defaultValue = "10", singleChar = 'w', longOption = "width")
   private int width = 10;
+  @CliParameter(defaultValue = "10", singleChar = 'h', longOption = "height")
   private int height = 10;
+  @CliParameter(defaultValue = "<now>", longOption = "seed")
   private String hashBase;
   private long seed = System.currentTimeMillis();
 
@@ -34,26 +46,118 @@ public class Config
     parseCommandline(args, 2);
   }
 
+  private final Map<CliParameter, Field> knownParameters = new HashMap<>();
+  private final Map<String, CliParameter> argNames = new HashMap<>();
+  private boolean legacyMode = false;
+
   public void parseCommandline(String[] args, int seedIndex)
   {
-    if(args.length > 0)
+    parseCommandline(args, null, seedIndex);
+  }
+
+  public void parseCommandline(String[] args, Class<?> parameterContext)
+  {
+    parseCommandline(args, parameterContext, -1);
+  }
+
+  public void parseCommandline(
+      String[] args, Class<?> parameterContext, int legacySeedIndex)
+  {
+    loadCliParameters(parameterContext);
+
+    List<String> unknown = new ArrayList<>();
+    List<String> walker = Arrays.asList(args);
+
+    try
     {
-      width = Integer.parseInt(args[0]);
-    }
-    if(args.length > 1)
-    {
-      height = Integer.parseInt(args[1]);
-    }
-    if(args.length > seedIndex && !args[seedIndex].isEmpty())
-    {
-      hashBase = args[seedIndex];
-      try
+      boolean skipNext = false;
+      for(String arg : walker)
       {
-        seed = Long.parseLong(args[seedIndex]);
+        if(skipNext)
+        {
+          skipNext = false;
+          continue;
+        }
+        CliParameter parameter = argNames.get(arg);
+        if(parameter != null)
+        {
+          unknown = null;
+          Field field = knownParameters.get(parameter);
+          Class<?> type = field.getType();
+          if(!parameter.standalone())
+          {
+            skipNext = true;
+            String next = walker.get(walker.indexOf(arg) + 1);
+            if(type == Boolean.class || type == boolean.class)
+            {
+              field.setAccessible(true);
+              field.set(this, Boolean.parseBoolean(next));
+            }
+            else if(type == Integer.class || type == int.class)
+            {
+              field.setAccessible(true);
+              field.set(this, Integer.parseInt(next));
+            }
+            else if(type == String.class)
+            {
+              field.setAccessible(true);
+              field.set(this, next);
+            }
+            else
+            {
+              throw new IllegalArgumentException(
+                  "unknown type " + type.getName() + " for " + arg);
+            }
+          }
+          else
+          {
+            if(type == Boolean.class || type == boolean.class)
+            {
+              field.setAccessible(true);
+              field.set(this, true);
+            }
+            else
+            {
+              throw new IllegalArgumentException(
+                  "unknown type " + type.getName() + " for " + arg);
+            }
+          }
+        }
+        else if(unknown != null)
+        {
+          unknown.add(arg);
+        }
       }
-      catch(NumberFormatException ex)
+    }
+    catch(IllegalArgumentException | ReflectiveOperationException ex)
+    {
+      System.err.println(ex.toString());
+      System.exit(1);
+    }
+
+    if(unknown != null)
+    {
+      legacyMode = true;
+      String[] legacyArgs = unknown.toArray(String[]::new);
+      if(legacyArgs.length > 0)
       {
-        seed = args[seedIndex].hashCode();
+        width = Integer.parseInt(legacyArgs[0]);
+      }
+      if(legacyArgs.length > 1)
+      {
+        height = Integer.parseInt(legacyArgs[1]);
+      }
+      if(legacyArgs.length > legacySeedIndex && !legacyArgs[legacySeedIndex].isEmpty())
+      {
+        hashBase = legacyArgs[legacySeedIndex];
+        try
+        {
+          seed = Long.parseLong(legacyArgs[legacySeedIndex]);
+        }
+        catch(NumberFormatException ex)
+        {
+          seed = legacyArgs[legacySeedIndex].hashCode();
+        }
       }
     }
   }
@@ -84,5 +188,93 @@ public class Config
     parameters.put("width", width);
     parameters.put("height", height);
     return parameters;
+  }
+
+  private void loadCliParameters(Class<?> parameterContext)
+  {
+    scanClass(Config.class);
+
+    if(parameterContext != null)
+    {
+      scanClass(parameterContext);
+    }
+    else
+    {
+      String callerClassName = null;
+      StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+      for(int i = 1; i < stackTrace.length; i++)
+      {
+        if(!Config.class.getName().equals(stackTrace[i].getClassName()))
+        {
+          callerClassName = stackTrace[i].getClassName();
+          break;
+        }
+      }
+      try
+      {
+        Class<?> callerClass =
+            Thread.currentThread().getContextClassLoader().loadClass(callerClassName);
+        scanClass(callerClass);
+      }
+      catch(ClassNotFoundException ex)
+      {
+        System.err.println(ex.toString());
+      }
+    }
+  }
+
+  private void scanClass(Class<?> aClass)
+  {
+    for(Field field : aClass.getDeclaredFields())
+    {
+      CliParameter paramDef = field.getAnnotation(CliParameter.class);
+      if(paramDef != null)
+      {
+        knownParameters.put(paramDef, field);
+        String longName = paramDef.longOption();
+        if(!longName.isBlank())
+        {
+          argNames.putIfAbsent("--" + longName, paramDef);
+        }
+        char shortName = paramDef.singleChar();
+        if(shortName != '\0')
+        {
+          argNames.putIfAbsent("-" + shortName, paramDef);
+        }
+        else if(longName.isBlank())
+        {
+          argNames.putIfAbsent("--" + field.getName(), paramDef);
+        }
+      }
+    }
+  }
+
+  public boolean isLegacyMode()
+  {
+    return legacyMode;
+  }
+
+  /**
+   * Marks a command line parameter. The parameter can be a {@code String}, {@code int} or
+   * {@code boolean} (or their wrapper classes) and must be {@code static}. A value
+   * defined on command line will be written directly to the field.
+   *
+   * The package containing a class with CliParameter Fields must be opened to the
+   * {@code core} module.
+   * <pre>opens this.is.my.app to de.nigjo.maze.core;</pre>
+   *
+   * @see Config#parseCommandline(String[], Class)
+   */
+  @Target(ElementType.FIELD)
+  @Retention(RetentionPolicy.RUNTIME)
+  public static @interface CliParameter
+  {
+    boolean standalone() default false;
+
+    char singleChar() default '\0';
+
+    String longOption() default "";
+
+    public String defaultValue() default "";
   }
 }
